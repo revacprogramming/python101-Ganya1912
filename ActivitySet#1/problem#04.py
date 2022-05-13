3ae977231c64c63ef194b9853015cb510178af98
Ganya <97089502+Ganya1912@users.noreply.github.com>
2022-05-13 04:48:57 +0000

    completed

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -10,3 +10,22 @@ if h > 40 :
 else :
     pay=(r*h)
 print(pay)
+
+'''
+score = input("Enter Score: ")
+score = float(score)
+
+if score <= 1 and score >=0 :
+  if (score >=0.9) :
+    print('A')
+  elif (score >=0.8) :
+    print('B')
+  elif (score >=0.7) :
+    print('C')
+  elif (score >=0.6) :
+    print('D')
+  else :
+    print('F')
+else :
+  print('ERROR')
+'''
