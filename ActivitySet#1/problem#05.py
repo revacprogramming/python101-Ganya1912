b71986bf2b6fe8c6da92591af08320338654fa92
Ganya <97089502+Ganya1912@users.noreply.github.com>
2022-05-17 16:34:34 +0000

    completed

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -11,19 +11,3 @@ rte = float(input("Enter rate per hour? "))
 p = computepay(hrs, rte)
 print("Pay", p)
 '''
-score = input("Enter Score: ")
-score = float(score)
-
-if score <= 1 and score >=0 :
-  if (score >=0.9) :
-    print('A')
-  elif (score >=0.8) :
-    print('B')
-  elif (score >=0.7) :
-    print('C')
-  elif (score >=0.6) :
-    print('D')
-  else :
-    print('F')
-else :
-  print('ERROR')
--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,4 @@
-# Dictionaries
 
-filename = "dataset/mbox-short.txt"
+'''a=12
+x=>>2   y=a<<1
+print(x,y)'''
