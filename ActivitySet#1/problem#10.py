58a698ec81bef07535503bd5c2756d48618179a0
Ganya <97089502+Ganya1912@users.noreply.github.com>
2022-07-05 08:51:17 +0000

    done

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -2,3 +2,11 @@
 '''a=12
 x=>>2   y=a<<1
 print(x,y)'''
+
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    print(line)
+print("Done")
