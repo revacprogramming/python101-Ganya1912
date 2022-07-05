9bac40d4a0f2e7b4afabfb0d99e85239c0b88ea7
Ganya <97089502+Ganya1912@users.noreply.github.com>
2022-07-05 08:31:54 +0000

    done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,17 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fname = input("romeo.txt:")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line.rstrip()
+    t = line.split()
+    lst.append(t)
+
+lst1 = []
+for i in lst:
+    lst1 = lst1+ i
+lst1 = list(set(lst1))
+lst1.sort()        
+print(lst1
